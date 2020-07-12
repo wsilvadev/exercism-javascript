fd692ec206a0af71c8932b56e2297bec9fdaa923
Willian <wsilvadev@gmail.com>
2020-07-12 11:14:15 -0300

    feat(resistor-color): list the different band colors  look up the numerical value associated with a particular color band

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,26 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const colorCode = (color) => {
+  color.toLowerCase();
+  let num = 0;
+  COLORS.map((item, index) => {
+    if (color === item) return (num = index);
+  });
+  return num;
+};
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
